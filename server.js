f7739515a437030108d3141b080b748058bb39b2
James-Fraser1 <jdfraser2022@gmail.com>
2022-08-19 15:10:19 -0400

    added callback function for department table

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
 const express = require('express');
+const mysql = require('mysql2')
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'gyno-4128',
+        database: 'employees'
+    },
+    console.log('Connected to the employees database.')
+);
+
+db.query(`SELECT * FROM department`, (err, rows) => {
+    console.log(rows);
 });
 
-
-
-
-
-
-
-
-
-
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
